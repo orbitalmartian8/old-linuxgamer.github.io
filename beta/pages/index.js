2393ad28bdf10a1a280b6088deb9275acaf81705
CJ <83363242+LinuxGamer@users.noreply.github.com>
2022-11-28 11:29:13 +0000

    Added Beta

---
+++ beta/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Button from  'next/button';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>CJ (Linux Gamer)</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm CJ, I'm a Digital/Pixel Artist, and software developer.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <Link href="/about" passHref>
+        <Button variant="contained" color="secondary">About</Button>
+      </Link>
+    </Layout>
+  );
+}
