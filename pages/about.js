47cfa1376516e04f224d21353da123e7750d9a2c
CJ <83363242+LinuxGamer@users.noreply.github.com>
2022-11-29 17:56:21 +0000

    Added footer to about.js

--- pages/about.js
+++ pages/about.js
@@ -28,9 +28,36 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
       </section>
         <p>Hello, I'm CJ, I'm a Digital/Pixel Artist, and software developer. I do a lot of JavaScript, Python and HTML/CSS work, but I also dabble in C++ work too.</p>
-      {/* <Link href="/about" passHref>
-        <Button variant="contained" color="secondary">About</Button>
-      </Link> */}
+        <p>I use <Link href="https://spins.fedoraproject.org/en/kde/">Fedora Linux 36 KDE Spin</Link> and a variety of applications and extensions.</p>
+        <p></p>
+        <p></p>
+        <p>
+          List of applications and extensions that I use:
+          <br />
+          <b>Applications:</b>
+            <li>Web browser - <Link href="https://www.chromium.org/getting-involved/download-chromium/" target="_blank" rel="noreferrer">Chromium</Link></li>
+            <li>File manager (GUI) - <Link href="https://apps.kde.org/en-gb/dolphin/" target="_blank" rel="noreferrer">Dolphin</Link></li>
+            <li>File manager (terminal) - <Link href="https://ranger.github.io/" target="_blank" rel="noreferrer">Ranger</Link></li>
+            <li>Office suite - Libreoffice</li>
+            <li>PDF Viewer - Okular</li>
+            <li>Art/Graphic Design - Krita</li>
+            <li>Pixel art - LibreSprite</li>
+          <b>Browser Extensions:</b>
+            <li>Dark Reader</li>
+            <li>YouTube Enhancer</li>
+          <b>KDE Extensions</b>
+            <li>Dracula Global Theme</li>
+            <li>Dracula Cursor</li>
+            <li>Dracula Icons</li>
+        </p>
+      <footer>
+        <a href="/contact">
+          <button>
+            Contact Us
+          </button>
+        </a>
+        <p>Website made with ❤ by CJ (LinuxGamer) - Copyright ©️ 2022 CJ (LinuxGamer)</p>
+      </footer>
     </Layout>
   );
 }
