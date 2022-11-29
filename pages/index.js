f75761492cdae5c59c324ec5c79117a983b00dd4
CJ <83363242+LinuxGamer@users.noreply.github.com>
2022-11-29 16:30:00 +0000

    Started work on card grid on index.js for best artworks, etc

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,32 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm CJ, I'm a Digital/Pixel Artist, and software developer.</p>
       </section>
-      
+      <section className={utilStyles.grid}>
+        <div className={utilStyles.grid}>
+          <a href="https://nextjs.org/docs" className={utilStyles.card}>
+            <h3>Documentation &rarr;</h3>
+            Find in-depth information about Next.js features and API.
+          </a>
+          <a href="https://nextjs.org/learn" className={utilStyles.card}>
+            <h3>Learn &rarr;</h3>
+            Learn about Next.js in an interactive course with quizzes!
+          </a>
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={utilStyles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            Discover and deploy boilerplate example Next.js projects.
+          </a>
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={utilStyles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+          </a>
+        </div>
+        </section>
       {/* <Link href="/about" passHref>
         <Button variant="contained" color="secondary">About</Button>
       </Link> */}
