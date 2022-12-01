9ebb932c83743748e742401e69890ca8b7531044
CJ <charl.cj.monke@gmail.com>
2022-12-01 09:44:43 +0000

    404 page made

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout.js';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import Script from 'next/script.js';
+// import Button from  'next/button';
+<Script language="yaml">
+---
+permalink: /404.html
+---
+</Script>
+
+export default function Home() {
+  return (
+    <Layout home>
+    <main>
+      <Head>
+        <title>CJ (Linux Gamer)</title>
+      </Head>
+      <Link href="/" passHref>Back to Home &rarr;</Link>
+      
+    </main>
+    <footer>
+    <p>
+      <a href="/contact">
+        <button>
+          Contact Us
+        </button>
+      </a>
+        Website made with ❤ by CJ  |  Copyright ©️ 2022 CJ (LinuxGamer) <Link href="https://github.com/LinuxGamer/linuxgamer.github.io" target="_blank" rel="noreferrer" passHref><span class="emoji" data-emoji="f09b"></span></Link></p>
+      </footer>
+    </Layout>
+  );
+}
